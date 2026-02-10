6b91c84cd0c6a0196d61c7e100985a56e5c7da75
Ahmed Eltantawi <ahmedeltantawi73@gmail.com>
2026-02-10 19:02:20 +0200

    refactor the size of app

--- lib/helper/consts.dart
+++ lib/helper/consts.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xff2E465E);
-const String kAppIcon = 'assets/images/app_icon.png';
+const String kAppIcon = 'assets/images/splash.png';
 const String kMassagesCollection = 'massages';
 const String kCreatedAtCollection = 'createdAt';
 const String kFriendsCollection = 'users';
